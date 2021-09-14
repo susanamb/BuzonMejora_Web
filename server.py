3669b4c5525602120fca7a7a9b8fb8316665cde2
Susana Murillo <susana.murillo@uabc.edu.mx>
2021-09-14 13:25:47 -0700

    reset pass fixes

--- server.py
+++ server.py
@@ -38,6 +38,26 @@ def registro():
 
     return render_template("registrouser.html")
 
+@app.route('/saveuser', methods = ['POST'])
+def saveuser():
+    if request.method == 'POST':
+            correo = request.form['correo']
+            contrasena = request.form['contrasena']
+            concontrasena = request.form['confirmacontrasena']
+            cor = correo.endswith("@uabc.edu.mx")  
+
+            if contrasena != concontrasena: # si las contrasenas no coinciden
+                return render_template("registrouser.html", warnin = "claves")
+            elif not cor: # si el correo no es uabc
+                return render_template("registrouser.html", warnin = "correo")
+            elif contrasena == concontrasena and cor: #si los datos son correctos
+                try:
+                    auth.create_user_with_email_and_password(correo, contrasena)
+                    return render_template("registrouser.html", warnin = "success")
+                except:
+                    return render_template("registrouser.html", warnin = "Contraseña muy debil")
+
+
 #vista p/resetear el password
 @app.route('/resetpass')
 def resetpass():
@@ -109,7 +129,6 @@ def loginuser():
     if request.method == 'POST':
         correo = request.form['correo']
         contrasena = request.form['contrasena']
-
         
     try:
         user = auth.sign_in_with_email_and_password(correo, contrasena)
@@ -119,12 +138,20 @@ def loginuser():
     except:
         return render_template("login.html", user = "no")
 
+#para resetear el password
 @app.route("/resetpasswor", methods = ['POST'])
 def ressetpas():
     if request.method == 'POST':
-        correo = request.form['correo']
-        auth.send_password_reset_email(correo)
-    return "<h1>Listo, Revisa tu correo </h1><p><a href='/'>Volver al inicio</p>"
+        if request.form['correo']:
+            correo = request.form['correo']
+            
+            cor = correo.endswith("@uabc.edu.mx")  
+            if cor:
+                try:
+                    auth.send_password_reset_email(correo)
+                    return render_template("resetpass.html", user = "si")
+                except:
+                    return render_template("resetpass.html", user = "no")
 
 if __name__ == "__main__":
     print("running....")
